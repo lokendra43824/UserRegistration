48c2f07d77d1f8463c44f5596e8d5cefc6e78805
Lokendra <lokilokendra12345@gmail.com>
2020-10-05 11:18:49 +0530

    [lokenda] added a code to uc-2

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -7,19 +7,28 @@ namespace UserRegistration
     {
         static void Main(string[] args)
         {
-            string firstName;
+            string firstName,lastName;
             Console.WriteLine("Welcome to user registration");
             Console.WriteLine("Enter your first name: ");
             firstName = Console.ReadLine();
-            while (!(validateFirstName(firstName)))
+            while (!(validateName(firstName)))
             {
                 Console.WriteLine("Please enter a valid first name");
                 firstName = Console.ReadLine();
             }
             Console.WriteLine("You have entered a valid first name");
+            Console.WriteLine("Enter your Last name: ");
+            lastName = Console.ReadLine();
+            while (!(validateName(lastName)))
+            {
+                Console.WriteLine("Please enter a valid last name");
+                firstName = Console.ReadLine();
+            }
+            Console.WriteLine("You have entered a valid last name");
+
         }
-    
-    public static bool validateFirstName(string firstName)
+
+        public static bool validateName(string firstName)
     {
         string pattern = @"(^[A-Z]{1}[a-z]{2,}$)";
         Regex re = new Regex(pattern);
